376568e08f5d6b7e7103736c8f5f96fb39942118
Victor Quinn <victor@socialradar.com>
2014-09-09 11:19:55 -0400

    Initial commit with non-generator based batch request

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-koa-batch
-=========
+Batch Request middleware for [koa](http://koajs.com). Turn one request into many!
+=======
 
-Batch Request middleware for koa. Turn one request into many!
+Looking for [the ExpressJS version of this module](https://github.com/socialradar/batch-request)?
+
+A simple library for batching HTTP requests
+
+[View Documentation](http://batch-request.socialradar.com)
+
+[![Build Status](https://travis-ci.org/socialradar/batch-request.png?branch=master)](https://travis-ci.org/socialradar/batch-request) [![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
+
+## QuickStart
+
+Download via [NPM](http://npmjs.org)
+
+[![NPM](https://nodei.co/npm/batch-request.png?compact=true)](https://nodei.co/npm/batch-request/)
+
+then in your app
+
+    var batch = require('koa-batch').batch();
+
+    // Use Batch Request as middleware on an endpoint you want to service batch requests (example using [koa-router](https://github.com/alexmingoia/koa-router))
+    app.post('/batch', batch);
+
+
+Optionally use our included middleware to check the validity of your batch request
+
+    var batch = require('koa-batch').batch(),
+        validate = require('koa-batch').validate();
+
+    // Include the validate middleware before batch middleware
+    app.post('/batch', validate, batch);
+
+And that's it!
+
+Proudly written in Washington, D.C. by:
+
+[![SocialRadar](https://raw.github.com/socialradar/batch-request/master/social-radar-black-orange.png)](http://socialradar.com)
---
+++ test/batch.js
@@ -0,0 +1,378 @@
+// Batch tests
+
+process.env.NODE_ENV = 'test';
+
+var Chance = require('chance'),
+    chance = new Chance(),
+    expect = require('chai').expect,
+    request = require('supertest');
+
+describe('batch', function() {
+    var app;
+    var batch;
+
+    before(function(done) {
+      app = require('./helpers/app')();
+      batch = require('../lib/batch-request')();
+      done();
+    });
+
+    after(function(done) {
+      app.server.close(done);
+    });
+
+    describe('basic', function() {
+        it('looks good', function() {
+            expect(batch).to.be.a('function');
+        });
+    });
+
+    describe('test our app helper', function() {
+        it('has a /users/1/name endpoint', function(done) {
+            request(app)
+                .get('/users/1/name')
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.exist;
+                    done();
+                });
+        });
+    });
+
+    describe('basic', function() {
+        it('can handle a single request, without a method', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    getName: {
+                        url: 'http://localhost:3000/users/1/name'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.have.property('getName');
+                    expect(res.body.getName.statusCode).to.equal(200);
+                    expect(res.body.getName.body).to.be.a('string');
+                    done();
+                });
+        });
+
+      it('can batch to a relative path', function(done) {
+        request(app)
+          .post('/batch')
+          .send({
+            getName: {
+              url: '/users/1/name'
+            }
+          })
+          .expect(200, function(err, res) {
+            expect(err).to.not.exist;
+            expect(res.body).to.have.property('getName');
+            expect(res.body.getName.statusCode).to.equal(200);
+            expect(res.body.getName.body).to.be.a('string');
+            done();
+          });
+      });
+
+      it('will handle a POST correctly', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    getName: {
+                        method: 'POST',
+                        url: 'http://localhost:3000/users/1/name'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.have.property('getName');
+                    expect(res.body.getName.statusCode).to.equal(200);
+                    expect(res.body.getName.body).to.be.a('string');
+                    done();
+                });
+        });
+
+        it('will handle a POST with a body correctly', function(done) {
+            var first = chance.first();
+            request(app)
+                .post('/batch')
+                .send({
+                    getName: {
+                        method: 'POST',
+                        body: { first: first },
+                        json: true,
+                        url: 'http://localhost:3000/users/1/name'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.have.property('getName');
+                    expect(res.body.getName.statusCode).to.equal(200);
+                    expect(res.body.getName.body).to.be.a('string');
+                    expect(res.body.getName.body).to.equal(first);
+                    done();
+                });
+        });
+
+        it('will handle a PUT correctly', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    getName: {
+                        method: 'PUT',
+                        url: 'http://localhost:3000/users/1/name'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.have.property('getName');
+                    expect(res.body.getName.statusCode).to.equal(200);
+                    expect(res.body.getName.body).to.be.a('string');
+                    done();
+                });
+        });
+
+        it('will handle deeply serialized objects on POST correctly', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    getName: {
+                        method: 'POST',
+                        url: 'http://localhost:3000/users/1/deep'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.have.property('getName');
+                    expect(res.body.getName.statusCode).to.equal(200);
+                    expect(res.body.getName.body).to.be.a('string');
+                    var obj = JSON.parse(res.body.getName.body);
+                    expect(obj.mixed.deep.foo).to.equal('bar');
+                    done();
+                });
+        });
+
+        it('will send back headers', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    getName: {
+                        method: 'POST',
+                        url: 'http://localhost:3000/users/1/deep'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.body).to.have.property('getName');
+                    expect(res.body.getName.statusCode).to.equal(200);
+                    expect(res.body.getName.body).to.be.a('string');
+                    expect(res.body.getName).to.have.property('headers');
+                    var obj = JSON.parse(res.body.getName.body);
+                    expect(obj.mixed.deep.foo).to.equal('bar');
+                    done();
+                });
+        });
+
+        describe('can handle multiple requests', function() {
+            it('without a method', function(done) {
+                request(app)
+                    .post('/batch')
+                    .send({
+                        getName: {
+                            url: 'http://localhost:3000/users/1/name'
+                        },
+                        getEmail: {
+                            url: 'http://localhost:3000/users/1/email'
+                        },
+                        getCompany: {
+                            url: 'http://localhost:3000/users/1/company'
+                        }
+                    })
+                    .expect(200, function(err, res) {
+                        expect(err).to.not.exist;
+                        expect(res.body).to.have.property('getName');
+                        expect(res.body.getName.statusCode).to.equal(200);
+                        expect(res.body.getName.body).to.be.a('string');
+                        expect(res.body.getEmail.statusCode).to.equal(200);
+                        expect(res.body.getEmail.body).to.be.a('string');
+                        expect(res.body.getCompany.statusCode).to.equal(200);
+                        expect(res.body.getCompany.body).to.be.a('string');
+                        done();
+                    });
+            });
+
+            it('one of which is bogus', function(done) {
+                request(app)
+                    .post('/batch')
+                    .send({
+                        getName: {
+                            url: 'http://localhost:3000/users/1/name'
+                        },
+                        getEmail: {
+                            url: 'http://localhost:3000/users/1/' + chance.word()
+                        },
+                        getCompany: {
+                            url: 'http://localhost:3000/users/1/company'
+                        }
+                    })
+                    .expect(200, function(err, res) {
+                        expect(err).to.not.exist;
+                        expect(res.body).to.have.property('getName');
+                        expect(res.body.getName.statusCode).to.equal(200);
+                        expect(res.body.getName.body).to.be.a('string');
+                        expect(res.body.getEmail.statusCode).to.equal(404);
+                        expect(res.body.getCompany.statusCode).to.equal(200);
+                        expect(res.body.getCompany.body).to.be.a('string');
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe('dependencies', function() {
+        it('will run multiple queries in parallel if no dependencies specified', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    time1: {
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    },
+                    time2: {
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    },
+                    time3: {
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    var now = new Date().getTime();
+                    expect(res.body.time1.body).to.be.within(now - 100, now + 100);
+                    expect(res.body.time2.body).to.be.within(now - 100, now + 100);
+                    expect(res.body.time3.body).to.be.within(now - 100, now + 100);
+                    done();
+                });
+
+        });
+
+        it('will run a dependency before its dependent', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    time1: {
+                        url: 'http://localhost:3000/users/1/delay'
+                    },
+                    time2: {
+                        dependency: 'time1',
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    var now = new Date().getTime();
+                    // Expect first one to finish within
+                    expect(res.body.time1.body).to.be.within(now - 1000, now + 1000);
+                    expect(res.body.time2.body).to.be.above(res.body.time1.body + 500);
+                    done();
+                });
+
+        });
+
+        it('will not choke on an empty string dependency', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    time1: {
+                        url: 'http://localhost:3000/users/1/delay'
+                    },
+                    time2: {
+                        dependency: '',
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    var now = new Date().getTime();
+                    // Expect first one to finish within
+                    expect(res.body.time1.body).to.be.ok;
+                    expect(res.body.time2.body).to.be.ok;
+                    done();
+                });
+
+        });
+
+        it('will run chained dependencies, in order', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    time1: {
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    },
+                    time2: {
+                        dependency: 'time1',
+                        url: 'http://localhost:3000/users/1/delay'
+                    },
+                    time3: {
+                        dependency: 'time2',
+                        url: 'http://localhost:3000/users/1/delay'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    var now = new Date().getTime();
+                    expect(res.body.time1.body).to.be.within(now - 1100, now + 1100);
+                    expect(res.body.time2.body).to.be.above(res.body.time1.body + 999);
+                    expect(res.body.time3.body).to.be.above(res.body.time2.body + 999);
+                    done();
+                });
+
+        });
+
+        it('can run a rather complex chain of dependencies, in order', function(done) {
+            request(app)
+                .post('/batch')
+                .send({
+                    time1: {
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    },
+                    time2: {
+                        dependency: 'time1',
+                        url: 'http://localhost:3000/users/1/delay'
+                    },
+                    time3: {
+                        dependency: 'time2',
+                        url: 'http://localhost:3000/users/1/hammertime'
+                    },
+                    time4: {
+                        dependency: 'time1',
+                        url: 'http://localhost:3000/users/1/delay'
+                    },
+                    time5: {
+                        dependency: 'time4',
+                        url: 'http://localhost:3000/users/1/delay'
+                    },
+                    time6: {
+                        dependency: 'time4',
+                        url: 'http://localhost:3000/users/1/delay'
+                    },
+                    time7: {
+                        dependency: 'time4',
+                        url: 'http://localhost:3000/users/1/delay'
+                    }
+                })
+                .expect(200, function(err, res) {
+                    expect(err).to.not.exist;
+                    var now = new Date().getTime();
+                    expect(res.body.time1.body).to.be.within(now - 1100, now + 1100);
+                    expect(res.body.time2.body).to.be.above(res.body.time1.body + 999);
+                    expect(res.body.time3.body).to.be.above(res.body.time2.body + 999);
+                    expect(res.body.time4.body).to.be.above(res.body.time1.body + 999);
+                    expect(res.body.time5.body).to.be.above(res.body.time4.body + 999);
+                    expect(res.body.time6.body).to.be.above(res.body.time4.body + 999);
+                    expect(res.body.time7.body).to.be.above(res.body.time4.body + 999);
+                    done();
+                });
+
+        });
+    });
+});
